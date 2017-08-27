e5a9fc85bcd96ec4f62e517aaa64adbb9b830e88
unknown <satya.wagle@gmail.com>
2017-08-27 12:23:30 -0500

    Installed Express framework for server development

--- Client_Side/main.js
+++ Client_Side/main.js
@@ -1,5 +1,12 @@
-function test(){
+function sendreq(){
 	var x = document.getElementById("user").value;
     var text = x;
-    document.getElementById("demo").innerHTML = text;
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() { 
+        if (req.readyState == 4 && req.status == 200)
+            callback(req.responseText);
+    }
+    req.open('GET', "localhost:8000/what.html", true);
+    req.send(null);
+    alert(text);
 }
